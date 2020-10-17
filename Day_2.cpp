3e79c11a894cc5a3661c533df80c2ff345b62331
Amardeep Bhagat <amardeepbhagat66@gmail.com>
2020-10-17 16:31:25 +0530

    "closes:#1"
    added cpp solution for 30 Days of code for Day 2,3,5,7,9,10,11,12,20 and 24

---
+++ Day_2.cpp
@@ -0,0 +1,32 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the solve function below.
+void solve(double meal_cost, int tip_percent, int tax_percent) {
+  double tip= (tip_percent*meal_cost)/100 ;
+   double tax= (meal_cost* tax_percent)/100;
+  double totalcost=  meal_cost + tip + tax;
+
+   cout<<round(totalcost);
+}
+
+int main()
+{
+    double meal_cost;
+    cin >> meal_cost;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int tip_percent;
+    cin >> tip_percent;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int tax_percent;
+    cin >> tax_percent;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    solve(meal_cost, tip_percent, tax_percent);
+
+    return 0;
+}
